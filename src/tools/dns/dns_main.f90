e0c5e300c71638eac9ef865259b645dc130313c0
jkostelecky <j.kostelecky@posteo.de>
2025-01-31 18:29:07 +0100

    merge

--- src/tools/dns/dns_main.f90
+++ src/tools/dns/dns_main.f90
@@ -47,7 +47,9 @@ program DNS
     use Avg_Spatial, only: IO_READ_AVG_SPATIAL, IO_WRITE_AVG_SPATIAL
     implicit none
     save
+ #ifdef USE_APU   
     !$omp requires unified_shared_memory
+ #endif
     ! -------------------------------------------------------------------
     character(len=32) fname, str
     integer ig
