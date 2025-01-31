e75a44b977bd1b5ad04cf5c31985532d8a261f8f
jkostelecky <j.kostelecky@posteo.de>
2025-01-31 18:51:24 +0100

    tiny change

--- src/tools/dns/dns_main.f90
+++ src/tools/dns/dns_main.f90
@@ -47,9 +47,9 @@ program DNS
     use Avg_Spatial, only: IO_READ_AVG_SPATIAL, IO_WRITE_AVG_SPATIAL
     implicit none
     save
- #ifdef USE_APU   
+    ! for APUs!
     !$omp requires unified_shared_memory
- #endif
+
     ! -------------------------------------------------------------------
     character(len=32) fname, str
     integer ig
